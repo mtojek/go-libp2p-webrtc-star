3362e57b66e91f48c1baec96a3296d072b066507
Marcin Tojek <tojek.m@gmail.com>
2019-09-01 17:13:02 +0200

    Basic stream implementation

--- stream.go
+++ stream.go
@@ -1,35 +1,41 @@
 package star
 
 import (
-	"time"
-
 	"github.com/libp2p/go-libp2p-core/mux"
+	"github.com/pion/datachannel"
+	"io"
+	"time"
 )
 
 type stream struct {
-	id string
+	id          string
+	dataChannel datachannel.ReadWriteCloser
 }
 
 var _ mux.MuxedStream = new(stream)
 
-func newStream() *stream {
+func newStream(dataChannel datachannel.ReadWriteCloser) *stream {
 	return &stream{
-		id: createRandomID("stream"),
+		id:          createRandomID("stream"),
+		dataChannel: dataChannel,
 	}
 }
 
-func (s *stream) Read(p []byte) (n int, err error) {
-	panic("implement me") // TODO
+func (s *stream) Read(p []byte) (int, error) {
+	i, err := s.dataChannel.Read(p)
+	if err != nil {
+		return i, io.EOF
+	}
+	return i, nil
 }
 
 func (s *stream) Write(p []byte) (n int, err error) {
-	panic("implement me") // TODO
+	return s.dataChannel.Write(p)
 }
 
 func (s *stream) Reset() error {
 	logger.Debugf("%s: Reset stream", s.id)
-
-	panic("implement me") // TODO
+	return s.dataChannel.Close()
 }
 
 func (s *stream) Close() error {
